46973914853dfa960258247bf5d136748250f5ee
Francesco Pandolfi <francesco.pan@gmail.com>
2020-04-14 10:39:53 +0200

    apartment controller and model

---
+++ app/Http/Controllers/ApartmentController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Apartment;
+use Illuminate\Http\Request;
+
+class ApartmentController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Apartment $apartments)
+    {
+        //
+    }
+
+
+    public function edit(Apartment $apartments)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Apartment $apartments)
+    {
+        //
+    }
+
+ 
+    public function destroy(Apartment $apartments)
+    {
+        //
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->date('birth_date');
             $table->rememberToken();
             $table->timestamps();
         });
