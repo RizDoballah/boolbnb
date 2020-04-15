fe66bbdb98150d7ca61850daf63361f842064c92
Francesco Pandolfi <francesco.pan@gmail.com>
2020-04-15 13:09:48 +0200

    Edit in progress

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -10,20 +10,6 @@ use Illuminate\Support\Facades\Storage;
 
 class ApartmentController extends Controller
 {
-    public function __construct()
-    {
-        $this->validateRules = [
-            'title'=> 'required',
-            'description'=> 'required|string|max:1000',
-            'lat'=> 'numeric',
-            'lon'=> 'numeric',
-            'main_img'=>'required|image',
-            'square_meters'=>'required|numeric|min:40|max:500',
-            'rooms'=>'required|numeric|min:1|max:10',
-            'bathrooms'=>'required|numeric|min:1|max:3',
-            'user_id'=>'exists:users,id',
-        ];
-    }
 
     public function index()
     {
@@ -34,34 +20,13 @@ class ApartmentController extends Controller
 
     public function create()
     {
-        return view('host.create');
+        
     }
 
 
     public function store(Request $request)
 
     {
-      $request->validate($this->validateRules);
-
-
-      $data = $request->all();
-      dd($data);
-
-       $path = Storage::disk('public')->put('images', $data['main_img'] );
-
-      $apartment = Apartment::create([
-            'user_id' => Auth::id(),
-            'title' => $data['title'],
-            'description' => $data['description'],
-            'main_img' => $path,
-            'square_meters'=>$data['square_meters'],
-            'rooms'=>$data['rooms'],
-            'bathroom'=>$data['bathroom']
-
-        ]);
-
-        return view('guest.show', compact('apartment'));
-
 
     }
 
