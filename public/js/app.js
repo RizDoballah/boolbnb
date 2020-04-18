965500d703a067a9ed10d40fc668acc7e6cca63d
Ghezlene Doballah <ghyzounette@gmail.com>
2020-04-18 09:12:07 +0200

    footer in progress

--- public/js/app.js
+++ public/js/app.js
@@ -49967,8 +49967,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\mump_project\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\mump_project\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ASUS\Desktop\boolean\boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ASUS\Desktop\boolean\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
                     <input id="lat" type="hidden" name="lat" value="">
                     <input id="lon" type="hidden" name="lon" value="">
                     <button id="search" type="submit">Cerca</button>
-  
+
                   </form>
                 </div>
               </div>
@@ -29,18 +29,106 @@
         </header>
         @else
         @include('layouts._menu')
-        @endif  
+        @endif
 
-        
+
       <main class="py-4">
             @yield('content')
       </main>
 
       <footer>
+        <div class="footer_top container">
+          <div class="row">
+            <div class="col-3">
+              <ul class="footer_list">
+                <li><h5 class="footer_titles">INFORMAZIONI</h5></li>
+                <li>
+                  <a href="#">Diversità e appartenenza</a>
+                </li>
+                <li>
+                  <a href="#">Accessibilità</a>
+                </li>
+                <li>
+                  <a href="#">Affidabilità e sicurezza</a>
+                </li>
+                <li>
+                  <a href="#">Airbnb Citizen</a>
+                </li>
+                <li>
+                  <a href="#">Newsroom</a>
+                </li>
+              </ul>
+            </div>
+              <div class="col-3">
+                <ul class="footer_list">
+                  <li><h5 class="footer_titles">COMMUNITY</h5></li>
+                  <li>
+                    <a href="#">Airbnb Magazine</a>
+                  </li>
+                  <li>
+                    <a href="#">Airbnb for Work</a>
+                  </li>
+                  <li>
+                    <a href="#">Invita degli amici</a>
+                  </li>
+                  <li>
+                    <a href="#">Opportunità di lavoro</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-3">
+                <ul class="footer_list">
+                  <li><h5 class="footer_titles">OSPITA</h5></li>
+                  <li>
+                    <a href="#">Diventa un host</a>
+                  </li>
+                  <li>
+                    <a href="#">Proponi un'esperienza</a>
+                  </li>
+                  <li>
+                    <a href="#">Affittare responsabilmente</a>
+                  </li>
+                  <li>
+                    <a href="#">Open Homes</a>
+                  </li>
+                  <li>
+                    <a href="#">Olimpiadi</a>
+                  </li>
+                  <li>
+                    <a href="#">Centro risorse</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-3">
+                <ul class="footer_list">
+                  <li><h5 class="footer_titles">ASSISTENZA</h5></li>
+                  <li>
+                    <a href="#">Centro Assistenza</a>
+                  </li>
+                  <li>
+                    <a href="#">Servizio di assistenza di quartiere</a>
+                  </li>
+                  <li>
+                    <a href="#">Affidabilità e sicurezza</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer_bottom container">
+            <div class="text">
+              <ul class="list-inline">
+                <li class="list-inline-item"><h6>© 2020 Airbnb, Inc</h6></li>
+                <li class="list-inline-item"><a href="#">All rights reserved</a></li>
+                <li class="list-inline-item"><a href="#">Termini</a></li>
+                <li class="list-inline-item"><a href="#">Mappa del sito</a></li>
+                <li class="list-inline-item"><a href="#">Dettagli dell'azienda</a></li>
+              </ul>
+            </div>
 
+          </div>
       </footer>
-
     </div>
-    <script src="{{asset('js/app.js')}}"></script> 
+    <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
