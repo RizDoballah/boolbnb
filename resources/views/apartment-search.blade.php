4974d825c47894d93f4e62f66a8d289c11f22e3f
Ghezlene Doballah <ghyzounette@gmail.com>
2020-04-20 11:28:10 +0200

    form

--- resources/views/apartment-search.blade.php
+++ resources/views/apartment-search.blade.php
@@ -2,7 +2,19 @@
 @section('content')
 <div class="container-fluid coord" data-lat="{{$coord['lat']}}" data-lon="{{$coord['lon']}}" >
     <div class="row">
+
         <div class="col-6">
+
+          <div class="filters">
+            <h1>Display a Range Field</h1>
+            <form action="/action_page.php">
+              <label for="vol">Volume (between 0 and 50):</label>
+              <input type="range" id="vol" name="vol" min="0" max="50">
+              <input type="submit">
+            </form>
+
+
+          </div>
 
             @foreach ($result as $apartment)
             <div class="row">
