5665696e939336b16055afee3cb774180f8b448e
Matteo <matteop18@gmail.com>
2020-04-30 18:29:17 +0200

    inizio show

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -55,10 +55,26 @@ class ApartmentSeeder extends Seeder
             'Roma', 'Firenze', 'Bologna', 'Milano', 'Rimini', 'Napoli', 'Palermo', 'Venezia', 'Siena', 'Terni', 'Perugia', 'Assisi'
           ];
 
+          $descriptionHome = [
+            'Appartamento di lusso, situato nel complesso Romana Playa, di fronte alla spiaggia. Dispone di 5 piscine, aree verdi, connessione internet simmetrica a 300 mbps in fibra ottica con Wi-Fi ad alta velocità, parcheggio e tutti i comfort e le strutture necessarie per una vacanza di lusso. Una grande zona barbecue, situata nel cortile di casa, può essere un luogo perfetto per il tempo della famiglia e degli amici. Il progetto offre spazio per sauna e palestra.',
+
+            'Eccezionale attico di lusso sulla spiaggia in prima linea sul New Golden Mile, tra Puerto Banus ed Estepona. Un grandissimo sviluppo di residenze straordinariamente grandi di 3 e 4 camere da letto, proprio di fronte al Mar Mediterraneo. Lo sviluppo è già costruito. Con le finestre dal pavimento al soffitto e quelle che sembrano essere infinite terrazze, ognuno degli edifici è la definizione di lusso, divertimento e viste indimenticabili sul mare. Situato su una spiaggia di fronte a un terreno di quasi 20.000 m2, questo complesso ad alta sicurezza con accesso esclusivo alla spiaggia.',
+
+            'Questa villa moderna di nuova costruzione si trova vicino al mare, a soli 100 m dalla spiaggia. Villa Alegria è costruita su un ampio terreno di 1 640 m² ed è composta da 5 camere da letto con bagno interno, ampio soggiorno e un ufficio. La sua caratteristica è una splendida piscina privata lunga 18 metri. Una grande zona barbecue, situata nel cortile di casa, può essere un luogo perfetto per il tempo della famiglia e degli amici. Il progetto offre spazio per sauna e palestra.',
+
+            "Questa lussuosa villa in vendita in Sierra Blanca offre le migliori viste panoramiche sul mare. L'imponente villa è stata costruita secondo i più alti standard e utilizzando i migliori materiali e finiture italiani. Situato nella zona più prestigiosa di Marbella: la Sierra Blanca, dove la privacy, la sicurezza e lo splendido paesaggio sono le caratteristiche principali. L'incredibile proprietà è stata rinnovata nel 2014 secondo i più alti standard. All'interno della villa, c'è un meraviglioso ingresso con un'ampia scala che rivela il livello di lusso della proprietà.",
+
+            'Questa moderna proprietà in vendita si trova a La Reserva de Alcuzcuz (Benahavis), nella parte occidentale di Marbella. La villa eccezionale offre grande privacy, alta qualità e standard di efficienza energetica e magnifiche viste panoramiche sul Mar Mediterraneo. Dispone di 5 camere da letto, 6 bagni (5 en-suite), 3 bagni per gli ospiti, cucina moderna completamente attrezzata con tutti gli elettrodomestici, aria condizionata, riscaldamento a pavimento in tutta la proprietà, ampie terrazze coperte e non coperte, piscina a sfioro, area cinema, bar dello sport, area giochi con tavolo da biliardo, palestra completamente attrezzata, zona benessere con sale per trattamenti, aree relax e bagno.'
+
+          ];
+
+        
+
+
           $newApartment = new Apartment;
           $newApartment->user_id= User::inRandomOrder()->first()->id;
           $newApartment->title = $titles[array_rand($titles)];
-          $newApartment->description = $faker->realtext(1000);
+          $newApartment->description = $descriptionHome[array_rand($descriptionHome)];
           $newApartment->rooms = rand(1, 3);
           $newApartment->beds = rand(1, 3);
           $newApartment->bathroom = rand(1, 3);
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-{{-- @dd($apartments) --}}
 <div class="container-fluid">
     <div class="row">
         <div class="col-12 show_image">
             <a href="{{route('home.show', $apartment)}}">
                 <img data-lat="{{$apartment->lat}}" data-lon="{{$apartment->lon}}" class="coord addPin w-100" src="{{asset('storage/' . $apartment->main_img)}}" alt="">
-                {{-- <img class="apartment_img" src="{{asset('storage/' . $apartment->main_img)}}" alt=""> --}}
             </a>
         </div>
     </div>
@@ -16,32 +14,33 @@
     <div class="row">
         <div class="col-lg-7 col-md-12 col-xs-12">
             <h1 class="h1_show_title">{{$apartment->title}}</h1>
-            <ul class="list-inline">
+            <small class="city-home">{{$apartment->city}}</small>
+            <ul class="list-inline mt-3">
                 <li class="list-inline-item">{{$apartment->square_meters}} Mq</li>
                 <li class="list-inline-item"> {{$apartment->rooms}} Camere</li>
                 <li class="list-inline-item">{{$apartment->bathroom}} Bagni </li>
                 <li class="list-inline-item">{{$apartment->beds}} Letti </li>
             </ul>
             <p>{{$apartment->description}}</p>
+            <div class="row">
+                <div class="col">
+                    <h4 class="mt-3">Servizi</h4>
+                    @foreach ($apartment->services as $service)
+                        <button type="button" class="btn btn-secondary btn-lg" disabled>{{$service->name}}</button>
+                    @endforeach
+                </div>
+            </div>
+            @if($apartment->user_id != Auth::id())
+                <button type="button" class="btn btn-primary my-5" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Invia un messaggio</button>
+            @endif
         </div>
 
         <div class="col-lg-5 col-md-12 col-xs-12">
-            <h3>La posizione</h3>
+            <h3 class="font-weight-bold mb-3">La posizione</h3>
             <div id='map' class='map_show'></div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col">
-            <h4 class="mt-3">Servizi</h4>
-            @foreach ($apartment->services as $service)
-                <button type="button" class="btn btn-secondary btn-lg" disabled>{{$service->name}}</button>
-            @endforeach
-        </div>
-    </div>
-    @if($apartment->user_id != Auth::id())
-        <button type="button" class="btn btn-primary my-5" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Invia un messaggio</button>
-    @endif
 </div>
 
     {{-- ***************************************** --}}
