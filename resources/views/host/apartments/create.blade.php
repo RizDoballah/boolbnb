82c28b2f48725432ad6a0e25adca6d53e10da1d1
Francesco Pandolfi <francesco.pan@gmail.com>
2020-04-30 18:04:14 +0200

    Create aggiornata

--- resources/views/host/apartments/create.blade.php
+++ resources/views/host/apartments/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-6">
+        <div class="col-md-6 col-sm-12">
             <form method='POST' action="{{route('host.store')}}" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
@@ -56,8 +56,7 @@
                     <h4>Quanto é grande il tuo allogio?</h4>
                     <label class="my-5 label_create" for="square_meters">Metri Quadri</label>
                     <span class='down_count btn' title='Down'><i class="fas fa-minus"></i></span>
-                    <input autocomplete="off" class='counter' name="square_meters" type="number" min="1"
-                        value="{{(empty(old('square_meters'))) ? '0' : old('square_meters')}}" />
+                    <input autocomplete="off" class='counter' name="square_meters" type="number" min="1" value="{{(empty(old('square_meters'))) ? '0' : old('square_meters')}}" />
                     <span class='up_count btn' title='Up'><i class="fas fa-plus"></i></span>
                     @error('square_meters')
                     <small class="form-text text-danger">{{$message}}</small>
@@ -65,11 +64,10 @@
                 </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-md-6 col-sm-12">
             <div class="form-group">
                 <h4 class="mt-5 mb-3">Dove si trova il tuo alloggio?</h4>
-                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror"
-                    name='address' placeholder="inserisci l'indirizzo" value="{{old('address')}}">
+                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror"name='address' placeholder="inserisci l'indirizzo" value="{{old('address')}}">
                 @error('address')
                 <small class="form-text text-danger">{{$message}}</small>
                 @enderror
@@ -88,104 +86,92 @@
 
             <div class="form-group">
                 <h4 class="mt-5 mb-3">Aggiungi foto al tuo annuncio</h4>
-                <input type="file" class="form-control-file @error('main_img') is-invalid @enderror" name="main_img"
-                    accept="image/*">
-                @error('main_img')
-                <small class="form-text text-danger">{{$message}}</small>
-                @enderror
-            </div>
-                <div class="form-group icon">
-                    <h4 class="mt-5 mb-3">Quali servizi metti a disposizione?</h4>
-                    <div class="container">
-                      <div class="row">
-                        <div class="col-6">
-                            <div class="checkbox_filter">
-                                <label class="container_checkbox">
-                                    <input class="form-check-input" name="services[]" value="Wi-fi"
-                                        {{-- {{(!empty($data['services']) && in_array('Wi-fi', $data['services'])) ? 'checked' : ''}} --}}
-                                        type="checkbox">
-
-                                    <i class="fas fa-wifi"></i> Wi-Fi
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-
-                            <div class="checkbox_filter">
-                                <label class="container_checkbox">
-                                    <input class="form-check-input" name="services[]" value="Piscina"
-                                        {{-- {{(!empty($data['services']) && in_array('Piscina', $data['services'])) ? 'checked' : ''}} --}}
-                                        type="checkbox">
-                                    <i class="fas fa-swimming-pool"></i> Piscina
-                                    <span class="checkmark"></span>
-                            </div>
-
-                            <div class="checkbox_filter">
-                                <label class="container_checkbox">
-                                    <input class="form-check-input" name="services[]" value="Posto macchina"
-                                        {{-- {{(!empty($data['services']) && in_array('Posto macchina', $data['services'])) ? 'checked' : ''}} --}}
-                                        type="checkbox">
-                                    <i class="fas fa-parking"></i> Posto macchina
-                                    <span class="checkmark"></span>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="checkbox_filter">
-                                <label class="container_checkbox">
-                                    <input class="form-check-input" name="services[]" value="Sauna"
-                                        {{-- {{(!empty($data['services']) && in_array('Sauna', $data['services'])) ? 'checked' : ''}} --}}
-                                        type="checkbox">
-
-                                    <i class="fas fa-hot-tub"></i> Sauna
-                                    <span class="checkmark"></span>
-                            </div>
-
-                            <div class="checkbox_filter">
-                                <label class="container_checkbox">
-                                    <input class="form-check-input" name="services[]" value="Vista mare"
-                                        {{-- {{(!empty($data['services']) && in_array('Vista mare', $data['services'])) ? 'checked' : ''}} --}}
-                                        type="checkbox">
-
-                                    <i class="fas fa-water"></i> Vista Mare
-                                    <span class="checkmark"></span>
-                            </div>
-
-                            <div class="checkbox_filter">
-                                <label class="container_checkbox">
-                                    <input class="form-check-input" name="services[]" value="Portineria"
-                                        {{-- {{(!empty($data['services']) && in_array('Portineria', $data['services'])) ? 'checked' : ''}} --}}
-                                        type="checkbox">
-
-                                    <i class="fas fa-concierge-bell"></i> Portineria
-                                    <span class="checkmark"></span>
-                            </div>
-                        </div>
-
-
-
-
-                    </div>
-                        
-
-
-
-
-
-                    @foreach ($services as $service)
-                    <input type="checkbox" name="services[]" value="{{$service->id}}">
-                    <span class="mr-4">{{$service->name}}</span>
-                    @endforeach
+                <div class="upload-btn-wrapper">
+                    <button type="submit" class="btn_foto"><i class="fas fa-cloud-upload-alt"></i> Carica foto</button>
+                    <input type="file" class="form-control-file @error('main_img') is-invalid @enderror" name="main_img" accept="image/*" >
+                    @error('main_img')
+                        <small class="form-text text-danger">{{$message}}</small>
+                    @enderror
                 </div>
             </div>
+                <div class="form-group icon">
+                  <h4 class="mt-5 mb-3">Quali servizi metti a disposizione?</h4>
+                  <div class="container services_create">
+                    <div class="row">
+                      <div class="col-6">
+                        <div class="checkbox_filter">
+                          <label class="container_checkbox">
+                            <input class="form-check-input" name="services[]" value="1"
+                            @if(is_array(old('services')) && in_array(1, old('services'))) checked @endif
+                            type="checkbox">
+                            <i class="fas fa-wifi"></i> Wi-Fi
+                            <span class="checkmark"></span>
+                          </label>
+                        </div>
 
+                            <div class="checkbox_filter">
+                              <label class="container_checkbox">
+                                <input class="form-check-input" name="services[]" value="3"
+                                @if(is_array(old('services')) && in_array(3, old('services'))) checked @endif
+                                type="checkbox">
+                                <i class="fas fa-swimming-pool"></i> Piscina
+                                <span class="checkmark create"></span>
+                              </label>
+                            </div>
+
+                            <div class="checkbox_filter">
+                              <label class="container_checkbox">
+                                <input class="form-check-input" name="services[]" value="2"
+                                @if(is_array(old('services')) && in_array(2, old('services'))) checked @endif
+                                type="checkbox">
+                                <i class="fas fa-parking"></i> Posto macchina
+                                <span class="checkmark"></span>
+                              </label>
+                            </div>
+                        </div>
+
+                        <div class="col-6">
+                          <div class="checkbox_filter">
+                            <label class="container_checkbox">
+                              <input class="form-check-input" name="services[]" value="5"
+                              @if(is_array(old('services')) && in_array(5, old('services'))) checked @endif
+                              type="checkbox">
+                              <i class="fas fa-hot-tub"></i> Sauna
+                              <span class="checkmark"></span>
+                            </label>
+                            </div>
+
+                            <div class="checkbox_filter">
+                              <label class="container_checkbox">
+                                <input class="form-check-input" name="services[]" value="6"
+                                @if(is_array(old('services')) && in_array(6, old('services'))) checked @endif
+                                type="checkbox">
+                                <i class="fas fa-water"></i> Vista Mare
+                                <span class="checkmark"></span>
+                              </label>
+                            </div>
+
+                            <div class="checkbox_filter">
+                              <label class="container_checkbox">
+                                <input class="form-check-input" name="services[]" value="4"
+                                @if(is_array(old('services')) && in_array(4, old('services'))) checked @endif
+                                type="checkbox">
+                                <i class="fas fa-concierge-bell"></i> Portineria
+                                <span class="checkmark"></span>
+                              </label>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
 
             <div class="form-group">
-                <select name="published">
+                <select id="published" name="published">
                     <option value="1">Pubblicato</option>
                     <option value="0">Non Pubblicato</option>
                 </select>
             </div>
-
-            <button id="save_apt" type='submit'>Salva</button>
+            <button id="save_apt" class="btn mt-4" type='submit'>Salva</button>
             </form>
         </div>
     </div>
